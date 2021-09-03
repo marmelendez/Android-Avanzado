9bd3fc6f6e620319b4749a36a9ca8b9172b862b9
Maribel Melendez <lizbethmelendez@live.com>
2021-09-02 21:08:24 -0500

    Create interface VehicleDao

--- Sesion4/app/src/main/java/org/bedu/roomvehicles/room/VehicleDao.kt
+++ Sesion4/app/src/main/java/org/bedu/roomvehicles/room/VehicleDao.kt
@@ -1,7 +1,29 @@
 package org.bedu.roomvehicles.room
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
 interface VehicleDao {
+    @Insert
+    fun insertVehicle(vehicle: Vehicle)
+
+    @Update
+    fun updateVehicle(vehicle: Vehicle)
+
+    @Delete
+    fun deleteVehicle(vehicle: Vehicle)
+
+    @Query("DELETE FROM Vehicle WHERE id=:id")
+    fun removeVehicleById(id: Int)
+
+    @Delete
+    fun removeVehicles(vararg vehicles: Vehicle)
+
+    @Query("SELECT * FROM Vehicle")
+    fun getVehicles(): List<Vehicle>
+
+    @Query("SELECT * FROM Vehicle WHERE id = :id")
+    fun getVehicleById(id: Int): Vehicle
+
+
 }
